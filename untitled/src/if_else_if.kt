a6c92cf54747ea053a577415b0482ab21aaaa979
BarrakBM <barrak_mandani@live.com>
2025-02-11 18:52:26 +0300

    Completed Kotlin If Statement exercises

---
+++ untitled/src/if_else_if.kt
@@ -0,0 +1,12 @@
+fun main(){
+
+    var num: Int = 0
+    if(num > 0){
+        println("The number is positive")
+    } else if (num < 0) {
+        println("The number is negative")
+    } else {
+        println("The number is zero")
+    }
+}
+
