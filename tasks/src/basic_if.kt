a3be2cd29ae1891f58770c8d1d0ee0a1dd1eaa81
mohammedsheshtar <112146380+mohammedsheshtar@users.noreply.github.com>
2025-02-11 19:30:51 +0300

    Added a checker for negative values for the basic if statement check

--- tasks/src/basic_if.kt
+++ tasks/src/basic_if.kt
@@ -3,4 +3,10 @@ fun main() {
     if (num > 0) {
         println("the number is positive");
     }
+    else if (num < 0) {
+        println("the number is negative");
+    }
+    else {
+        println("the number is 0");
+    }
 }
