f0c08c65ddc37b0531bf0f53de129f9e94db8b7f
Yousef Alothman <yousefoalothman@gmail.com>
2025-02-11 19:27:33 +0300

    "Added basic if statement check"

---
+++ tasks/src/if_else.kt
@@ -0,0 +1,19 @@
+fun main(){
+
+    var num: Int = -1
+
+    if (num > 0){
+
+        println("the number is positive")
+
+    }
+    else {
+
+        println("number is negative")
+
+    }
+
+
+
+
+}
