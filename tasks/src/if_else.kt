87a03599260844c9c705e6d082dfcf2b08d5d8b3
mohammedsheshtar <112146380+mohammedsheshtar@users.noreply.github.com>
2025-02-11 19:33:46 +0300

    Implemented if-else statement to check positive and negative numbers

--- tasks/src/if_else.kt
+++ tasks/src/if_else.kt
@@ -1,3 +1,12 @@
 fun main() {
-
+    var num: Int = 10;
+    if (num > 0) {
+        println("the number is positive");
+    }
+    else if (num < 0) {
+        println("the number is negative");
+    }
+    else {
+        println("the number is 0");
+    }
 }
