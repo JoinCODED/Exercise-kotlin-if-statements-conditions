ed99475c002ba226c5d6020b04a246c4eb950a3c
Yousef <yayalkandari@gmail.com>
2025-02-11 19:10:16 +0300

    fixed file naming for grade task and removed comments

--- Task/src/leap_year.kt
+++ Task/src/leap_year.kt
@@ -1,19 +1,3 @@
-//**Task:**
-//
-//Write a program that assigns a letter grade based on a student's score.
-//
-//**Steps:**
-//
-//1. Define an integer variable `score`.
-//2. Use an `if-else if-else` statement to determine the grade:
-//- If `score` is 90 or above, print `"Grade: A"`.
-//- If `score` is 80-89, print `"Grade: B"`.
-//- If `score` is 70-79, print `"Grade: C"`.
-//- If `score` is 60-69, print `"Grade: D"`.
-//- Otherwise, print `"Grade: F"`.
-//
-//**Expected Output Example:**
-
 fun main() {
  var score: Int = 89;
 
