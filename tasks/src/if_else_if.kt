e9585b7f4d99a4acd9b3fb504fe16521dd155cc9
Yousef Alothman <yousefoalothman@gmail.com>
2025-02-11 19:39:45 +0300

    "Implemented if-else statement to check positive and negative numbers"

---
+++ tasks/src/if_else_if.kt
@@ -0,0 +1,17 @@
+fun main() {
+
+    var num: Int = 0
+
+    if (num > 0){
+        println("The number is positive")
+
+    } else if (num < 0){
+
+        println("the number is negative")
+    } else {
+
+        println("the number is zero")
+
+    }
+
+}
