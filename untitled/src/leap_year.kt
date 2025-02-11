5fa101a546182b168fcf9baf4700cc0d75143b22
BarrakBM <barrak_mandani@live.com>
2025-02-11 18:57:38 +0300

    Implemented leap year checker

--- untitled/src/leap_year.kt
+++ untitled/src/leap_year.kt
@@ -2,7 +2,7 @@ fun main(){
 
     var years: Int = 2030
 
-    if(years % 4 == 0 && years % 100 != 0 || years % 400 != 0){
+    if(years % 4 == 0 && years % 100 != 0 || years % 400 == 0){
         println("Leap year")
     }else{
         println("not a Leap year")
