8cac386a42f523e8e28940a647dbb9dac7cf43c5
Motlaq-Alnassafi <alnassafimotlaq@gmail.com>
2025-02-11 19:08:25 +0300

    Implemented leap year checker

--- leap_year.kt
+++ leap_year.kt
@@ -1,7 +1,7 @@
 fun main() {
     var year = 2880
 
-    if ((year % 4 == 0) && ((year % 100 != 0) || (year % 400 != 0))) {
+    if ((year % 4 == 0) && (year % 100 != 0) && (year % 400 != 0)) {
         println("Leap Year")
     } else {
         println("Not a Leap Year")
