4a0c25ef7219402d57a360d5619a80e69ef7f11e
Yousef <yayalkandari@gmail.com>
2025-02-11 18:58:13 +0300

    Implemented program to find the largest of three numbers

--- Task/src/largest_number.kt
+++ Task/src/largest_number.kt
@@ -1,7 +1,16 @@
 fun main() {
     var a: Int = 4;
-    var b: Int = 3;
-    var c: Int = 2
+    var b: Int = 4;
+    var c: Int = 4;
 
+    if (a > b && a > c) {
+        println("The largest number is $a");
+    } else if (b > a && b > c) {
+        println("The largest number is $b");
+    } else if (c > a && c > b) {
+        println("The largest number is $c");
+    } else {
+        println("All Equal: $a");
+    }
 
 }
