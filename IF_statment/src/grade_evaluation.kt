4c52bbe70b592672a1d1bd2d87b3b5cad18e6377
Dalal Bakhsh <DalalBakhsh98@gmail.com>
2025-02-11 23:10:53 +0300

    Added grade evaluation program

---
+++ IF_statment/src/grade_evaluation.kt
@@ -0,0 +1,25 @@
+fun main()
+{
+    var score:Int = 77
+
+    if(score >= 90)
+    {
+        println("Grade: A")
+    }
+    else if(score >= 80)
+    {
+        println("Grade: B")
+    }
+    else if(score >= 70)
+    {
+        println("Grade: C")
+    }
+    else if(score >= 60)
+    {
+        println("Grade: D")
+    }
+    else
+    {
+        println("Grade: F")
+    }
+}
