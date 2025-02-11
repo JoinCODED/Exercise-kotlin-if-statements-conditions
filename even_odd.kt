212c789f045b358e1f400ec5ee2a802618bbe180
alrweashd <trkyabler@gmail.com>
2025-02-11 19:43:19 +0300

    Fourth  task

---
+++ even_odd.kt
@@ -0,0 +1,23 @@
+fun main(args: Array<String>) {
+
+    var num =30;
+
+    //postive
+    var condition = num % 2 == 0;
+    var message1 = "The number is even";
+    var message2 = "The number is odd";
+    // .javaClass.kotlin
+
+    /*    The temperature is 36.5 degrees.
+        Is it raining? false
+        The first letter of my name is A.*/
+
+    if (condition){
+        println("${message1}" );
+    }
+    else {
+        println("${message2}" );
+    }
+
+
+}
