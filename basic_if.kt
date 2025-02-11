7c0bcc9d4ecc4d53cf9c346dd9d032292d730a1b
alrweashd <trkyabler@gmail.com>
2025-02-11 19:37:28 +0300

    First and second task

---
+++ basic_if.kt
@@ -0,0 +1,19 @@
+fun main(args: Array<String>) {
+
+    var num =30;
+
+    //postive
+    var condition = num > 0;
+    var message = "The number is positive";
+    // .javaClass.kotlin
+
+    /*    The temperature is 36.5 degrees.
+        Is it raining? false
+        The first letter of my name is A.*/
+
+    if (condition){
+        println("${message}" );
+    }
+
+
+}
