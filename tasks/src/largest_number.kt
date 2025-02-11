bc71124af406385933e68ed56d35852ce0bc8f14
Yousef Alothman <yousefoalothman@gmail.com>
2025-02-11 20:16:24 +0300

    "Implemented program to find the largest of three numbers"

---
+++ tasks/src/largest_number.kt
@@ -0,0 +1,19 @@
+fun main(){
+
+    var a: Int = 30
+    var b: Int = 40
+    var c: Int = 50
+
+    if (a > b && a > c){
+
+        println("a is largest number")
+
+    } else if(b > c && b > a){
+
+        println("b is largest number")
+    } else {
+
+        println("c is largest number")
+    }
+
+}
