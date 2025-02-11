d01ce011d1d88b6717d1bd2af4d8d5bb29af7007
ahmedmandy <ahmedmandy@me.com>
2025-02-11 19:08:57 +0300

    Added grade evaluation program

---
+++ If Statements Conditions/src/grade_evaluation.kt
@@ -0,0 +1,14 @@
+fun main() {
+    var score = 59
+    if (score >= 90) {
+        println("Grade: A")
+    } else if (score >= 80 && score <= 89) {
+        println("Grade: B")
+    } else if (score >= 70 && score <= 79) {
+        println("Grade: C")
+    } else if (score >= 60 && score <= 69) {
+        println("Grade: D")
+    } else {
+        println("Grade: F")
+    }
+}
