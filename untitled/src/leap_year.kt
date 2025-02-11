bc23f5e6695444ab4370f8f64f826bdcf0728cf9
BarrakBM <barrak_mandani@live.com>
2025-02-11 20:42:02 +0300

    added nested loop

--- untitled/src/leap_year.kt
+++ untitled/src/leap_year.kt
@@ -2,8 +2,9 @@ fun main(){
 
     var years: Int = 2030
 
-    if(years % 4 == 0 && years % 100 != 0 || years % 400 == 0){
-        println("Leap year")
+    if(years % 4 == 0){
+        if(years % 100 != 0 || years % 400 == 0)
+            println("Leap year")
     }else{
         println("not a Leap year")
     }
