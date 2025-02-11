ae33acef30b6ace3f15797a84e6d8dfa4ad6cca5
BarrakBM <barrak_mandani@live.com>
2025-02-11 18:45:52 +0300

    Added grade evaluation program

---
+++ untitled/src/grade_evaluation.kt
@@ -0,0 +1,15 @@
+fun main(){
+
+    var score: Int = 85
+    if(score > 90){
+        println("Grade is A")
+    }else if(score >= 80) {
+        println("Grade is B")
+    }else if(score >= 70){
+        println("Grade is C")
+    }else if(score >= 60){
+        println("Grade is D")
+    }else{
+        println("Grade is F")
+    }
+}
