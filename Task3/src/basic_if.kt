160b828bec5533a9fa6db3abce94e2ba707719f9
wadha almarshed <wadhaalmarshed@hotmail.com>
2025-02-11 23:30:32 +0300

    Completed Kotlin If Statement exercises

---
+++ Task3/src/basic_if.kt
@@ -0,0 +1,9 @@
+fun main() {
+    var num: Int = 10
+
+    if( num > 0 )
+    {
+        print("The number is positive")
+    }
+
+}
---
+++ Task3/src/leap_year.kt
@@ -0,0 +1,15 @@
+fun main() {
+   var year: Int = 2016
+
+    if(year % 4 == 0)
+    {
+        println("Leap Year")
+    }
+    else
+    {
+        println("Not a Leap Year")
+    }
+
+
+
+}
