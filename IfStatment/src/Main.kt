c846745317b0c9246a5b184e0544283513a18aef
Rabab <raaa555@outlook.com>
2025-02-12 23:44:39 +0300

    implemented even or odd number checker

--- IfStatment/src/Main.kt
+++ IfStatment/src/Main.kt
@@ -1,16 +1,21 @@
 fun main() {
 
- //task1
-   var num = 5
+        //task1
+        val num = -1
 
-    if (num > 0) {
-        println("The number $num is postive")
+        if (num > 0) {
+            println("The number $num is postive")
+        } else if (num < 0) {
+
+            println("The number $num is negative")
+        } else
+            println("The number is zero")
+
+    val num2 = 8
+
+    if (num2 % 2 == 0 ) {
+        println("The number is even")
+    }else {
+        println("The number is odd")
     }
-    else {
-        println("The number $num is negative")
     }
-
-//task2
-
-    
-}
