bc495f53ca9c70933441650e342e12cc9d2fea24
abdulwahab alsenan <ak-alsenan@hotmail.com>
2025-02-12 12:11:47 +0300

    Added if, else-if statement to handle zero case

---
+++ if_else_if.kt
@@ -0,0 +1,12 @@
+//if_else_if
+fun main() {
+    var num = 1337
+
+    if (num > 0) {
+        println("The number is positive")
+    } else if (num < 0)
+        println("the number is negative")
+    else {
+        println("the number is zero")
+    }
+}
